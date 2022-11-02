81dcb7eef12a04998917e24c123d1f1679e78030
Tamilhp <tselvan482@gmail.com>
2022-11-02 12:16:13 +0530

    Pandas imported
    Demo

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-print("Hello")
-print("Krishna")
+import pandas as pd 
+import sqlite3
+
+with sqlite3.connect(r"E:\Tamil Selvan\Sqlite sevalayam\sponsors.db") as con:
+    command = "SELECT * FROM November WHERE `Occasion Date` = '2013-11-01 00:00:00'"
+    cur = con.execute(command)
+    for row in cur:
+        print(row)
