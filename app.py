2b6c94918e3e674bf7bbf16f75fd6514786d8933
Tamilhp <tselvan482@gmail.com>
2022-11-04 14:31:17 +0530

    Updated version

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def data():
         for key, value in form_data.items():
             values.append(value)
         with sqlite3.connect(r"E:\Tamil Selvan\Sqlite sevalayam\sponsors.db") as con:
-            command = "SELECT `Name`, `Mobile no 1`, `Occasion`, DATE(`Date`) FROM November_modified WHERE `Occasion Date` = ? AND `Occasion Month`= ?"
+            command = "SELECT `Name`, `Mobile no 1`,`Mobile no 2`, `Occasion`, DATE(`Date`) FROM November_modified WHERE `Occasion Date` = ? AND `Occasion Month`= ?"
             cur = con.execute(command, (values[0],values[1],))
-        return render_template('data.html',form_data = cur)
+        return render_template('display_data.html',form_data = cur)
  
  
 app.run(host='localhost', port=8080)
